ca5c2c8be495a133a1b2cf5d11b746066eec277f
Dennis Serkov <dennis@serkov.com>
2017-09-19 19:38:57 -0400

    corrected syntax

--- media.py
+++ media.py
@@ -1,11 +1,12 @@
 import webbrowser
 
-class Movie(): 
+
+class Movie():
     """ This class provides a way to store movie related information"""
     VALID_RATINGS = ["G", "PG", "PG-13", "R"]
 
-    def __init__(self, movie_title, movie_storyline, poster_image, 
-	         trailer_youtube):
+    def __init__(self, movie_title, movie_storyline, poster_image,
+                 trailer_youtube):
         self.title = movie_title
         self.storyline = movie_storyline
         self.poster_image_url = poster_image
@@ -13,3 +14,4 @@ class Movie():
 
     def show_trailer(self):
         webbrowser.open(self.trailer_youtube_url)
+
